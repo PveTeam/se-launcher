2494a21b9d8be34356599be3fa16ada7d22263e2
zznty <94796179+zznty@users.noreply.github.com>
2024-11-01 23:05:28 +0700

    correct default nuget sources

--- CringePlugins/Config/PackagesConfig.cs
+++ CringePlugins/Config/PackagesConfig.cs
@@ -8,8 +8,9 @@ namespace CringePlugins.Config;
 public record PackagesConfig(ImmutableArray<PackageSource> Sources, ImmutableArray<PackageReference> Packages)
 {
     public static PackagesConfig Default { get; } = new([
-        new("SpaceEngineersDedicated.ReferenceAssemblies", "https://nuget.storage.yandexcloud.net/index.json"),
-        new("ImGui.NET.DirectX", "https://nuget.storage.yandexcloud.net/index.json"),
+        new("SpaceEngineersDedicated.ReferenceAssemblies", "https://ng.zznty.ru/v3/index.json"),
+        new("ImGui.NET.DirectX", "https://ng.zznty.ru/v3/index.json"),
+        new("Plugin", "https://ng.zznty.ru/v3/index.json"),
         new(string.Empty, "https://api.nuget.org/v3/index.json")
     ], []);
 }
