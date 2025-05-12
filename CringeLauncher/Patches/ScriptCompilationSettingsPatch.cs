227dcb18694f56f170fecb1d95a249f60e718d41
zznty <94796179+zznty@users.noreply.github.com>
2025-05-13 01:33:59 +0700

    force compiler version to be latest available

---
+++ CringeLauncher/Patches/ScriptCompilationSettingsPatch.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection.Emit;
+using HarmonyLib;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using VRage.Scripting;
+
+namespace CringeLauncher.Patches;
+
+[HarmonyPatch(typeof(MyScriptCompiler), nameof(MyScriptCompiler.CreateCompilation))]
+public static class ScriptCompilationSettingsPatch
+{
+    private static readonly CSharpParseOptions Options = new(LanguageVersion.Latest, DocumentationMode.None);
+    
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        var field = AccessTools.Field(typeof(MyScriptCompiler), nameof(MyScriptCompiler.m_conditionalParseOptions));
+        return new CodeMatcher(instructions)
+            .Start()
+            .MatchStartForward(CodeMatch.IsLdarg(0), CodeMatch.LoadsField(field))
+            .SetAndAdvance(OpCodes.Nop, null)
+            .SetInstructionAndAdvance(CodeInstruction.LoadField(typeof(ScriptCompilationSettingsPatch), nameof(Options)))
+            .Instructions();
+    }
+}
