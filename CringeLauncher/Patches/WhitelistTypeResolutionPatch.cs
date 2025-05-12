4ac3989115fc9c6e7f4456806034d5bccbe83dad
zznty <94796179+zznty@users.noreply.github.com>
2025-05-12 18:51:44 +0700

    fix whitelist registration of generic type parameters

--- CringeLauncher/Patches/WhitelistTypeResolutionPatch.cs
+++ CringeLauncher/Patches/WhitelistTypeResolutionPatch.cs
@@ -1,6 +1,5 @@
-﻿using System.Diagnostics;
-using System.Reflection.Emit;
-using HarmonyLib;
+﻿using HarmonyLib;
+using Microsoft.CodeAnalysis;
 using VRage.Scripting;
 
 namespace CringeLauncher.Patches;
@@ -8,18 +7,37 @@ namespace CringeLauncher.Patches;
 [HarmonyPatch(typeof(MyScriptWhitelist.Batch), nameof(MyScriptWhitelist.Batch.ResolveTypeSymbol))]
 public static class WhitelistTypeResolutionPatch
 {
-    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    [HarmonyReversePatch]
+    private static INamedTypeSymbol ResolveTypeSymbol(MyScriptWhitelist.Batch batch, Type type) => throw null!;
+    
+    // cant be assed to write a transpiler so heres a prefix
+    private static bool Prefix(MyScriptWhitelist.Batch __instance, Type type, ref INamedTypeSymbol __result)
     {
-        var call = CodeInstruction.CallClosure((MyWhitelistException ex) =>
-        {
-            Debug.WriteLine(ex);
-        });
+        // fast path
+        if (!type.IsGenericType || !type.IsConstructedGenericType)
+            return true;
 
-        return instructions.Manipulator(i => i.opcode == OpCodes.Throw,
-            i =>
-            {
-                i.opcode = call.opcode;
-                i.operand = call.operand;
-            });
+        __result = ResolveGenericTypeSymbol(__instance, type);
+        return false;
+    }
+
+    private static INamedTypeSymbol ResolveGenericTypeSymbol(MyScriptWhitelist.Batch batch, Type type)
+    {
+        // if type is not generic or constructed generic, run regular lookup
+        if (!type.IsGenericType || !type.IsConstructedGenericType)
+            return ResolveTypeSymbol(batch, type);
+        
+        var unconstructedSymbol = ResolveTypeSymbol(batch, type.GetGenericTypeDefinition());
+        
+        var typeArguments = type.GetGenericArguments();
+        
+        var typeSymbolArguments = new ITypeSymbol[typeArguments.Length];
+        for (var i = 0; i < typeArguments.Length; i++)
+        {
+            // recursively resolve (possibly) generic arguments
+            typeSymbolArguments[i] = ResolveGenericTypeSymbol(batch, typeArguments[i]);
+        }
+        
+        return unconstructedSymbol.Construct(typeSymbolArguments);
     }
 }
