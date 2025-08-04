b24bf0b71a4937b59cca955915d37aa614562c04
zznty <94796179+zznty@users.noreply.github.com>
2025-08-04 21:58:55 +0700

    change xml root writer patch to be 100% accurate under crossgen

--- CringeLauncher/Patches/XmlRootWriterPatch.cs
+++ CringeLauncher/Patches/XmlRootWriterPatch.cs
@@ -1,69 +1,22 @@
 ﻿using System.Reflection;
 using System.Reflection.Emit;
-using System.Text;
-using System.Xml;
-using System.Xml.Serialization;
 using HarmonyLib;
-using VRage.ObjectBuilders;
-using VRage.ObjectBuilders.Private;
 
 namespace CringeLauncher.Patches;
 
-// doesn't work with crossgen enabled
-/*[HarmonyPatch(typeof(CustomRootWriter), "Init")]
+[HarmonyPatch(TypeName, "WriteStartAttribute")]
+[HarmonyPatch([typeof(string), typeof(string), typeof(string)])]
 public static class XmlRootWriterPatch
 {
-    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    private const string TypeName = "System.Xml.XmlWellFormedWriter, System.Private.Xml";
+    
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, MethodBase original)
     {
-        var ins = instructions.ToList();
-
-        var index = ins.FindIndex(b =>
-                                      b.opcode == OpCodes.Ldstr && b.operand is "xsi:type");
-        ins[index].operand = "xsi";
-
-        ins.InsertRange(index + 1,
-        [
-            new CodeInstruction(OpCodes.Ldstr, "type"),
-            new CodeInstruction(OpCodes.Ldstr, "http://www.w3.org/2001/XMLSchema-instance")
-        ]);
-
-        var instruction = ins[ins.FindIndex(b => b.opcode == OpCodes.Callvirt)];
-        instruction.operand = AccessTools.Method(typeof(XmlWriter), "WriteAttributeString",
-        [
-            typeof(string),
-            typeof(string),
-            typeof(string),
-            typeof(string)
-        ]);
-
-        return ins;
-    }
-}*/
-
-[HarmonyPatch]
-public static class XmlSerializerWriterPatch
-{
-    private static IEnumerable<MethodInfo> TargetMethods()
-    {
-        yield return AccessTools.DeclaredMethod(typeof(MyObjectBuilderSerializerKeen),
-            nameof(MyObjectBuilderSerializerKeen.SerializeXML),
-            [typeof(string), typeof(bool), typeof(MyObjectBuilder_Base), typeof(ulong).MakeByRefType(), typeof(Type)]);
-        yield return AccessTools.DeclaredMethod(typeof(MyObjectBuilderSerializerKeen), "SerializeXMLInternal");
-    }
-    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-    {
-        var method = AccessTools.DeclaredMethod(typeof(XmlSerializer), nameof(XmlSerializer.Serialize), [typeof(Stream), typeof(object)]);
-        var serialize = AccessTools.DeclaredMethod(typeof(XmlSerializerWriterPatch), nameof(Serialize));
-        return instructions.Manipulator(i => i.Calls(method), i =>
-        {
-            i.opcode = OpCodes.Call;
-            i.operand = serialize;
-        });
-    }
-
-    private static void Serialize(XmlSerializer serializer, Stream stream, object value)
-    {
-        var writer = new XmlTextWriter(stream, Encoding.UTF8);
-        serializer.Serialize(writer, value);
+        var method = AccessTools.DeclaredMethod(original.DeclaringType, "CheckNCName");
+        return new CodeMatcher(instructions)
+            .MatchStartForward(CodeMatch.LoadsArgument(), CodeMatch.Calls(method))
+            .SetAndAdvance(OpCodes.Nop, null)
+            .SetAndAdvance(OpCodes.Nop, null)
+            .InstructionEnumeration();
     }
 }
