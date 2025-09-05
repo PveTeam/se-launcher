c2d8f50a234150668af9d8971f49939f957c9123
pas2704 <pas2704@outlook.com>
2025-09-05 00:24:24 -0400

    Fix edge case with mod compilation

---
+++ CringeLauncher/Patches/LoadScriptsPatch.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using Sandbox.Game.World;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace CringeLauncher.Patches;
+
+[HarmonyPatch(typeof(MyScriptManager), "LoadScripts")]
+public static class LoadScriptsPatch
+{
+    [HarmonyTranspiler]
+    public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        var matcher = new CodeMatcher(instructions)
+            .SearchForward(i => i.opcode == OpCodes.Ldloc_1)
+            .SearchForward(i => i.opcode == OpCodes.Ret)
+            .Advance(1);
+
+        var call = matcher.Operand as MethodInfo ?? throw new InvalidOperationException("Changes to LoadScripts");
+
+        return matcher.SetAndAdvance(OpCodes.Ldloc_1, null)
+            .InsertAndAdvance(CodeInstruction.CallClosure((string[] s) => Array.Sort(s)))
+            .Insert(new CodeInstruction(OpCodes.Call, call))
+            .InstructionEnumeration();
+    }
+}
