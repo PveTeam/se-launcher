38122364ffcd2139d7964c13aaa3c740dfad9a9d
zznty <94796179+zznty@users.noreply.github.com>
2025-09-12 21:56:12 +0700

    fix entity creation thread preventing process from exiting during crash

---
+++ CringeLauncher/Patches/EntityCreationThreadPriorityPatch.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection.Emit;
+using HarmonyLib;
+using Sandbox.Game.Entities;
+
+namespace CringeLauncher.Patches;
+
+[HarmonyPatch(typeof(MyEntityCreationThread), MethodType.Constructor)]
+internal static class EntityCreationThreadPriorityPatch
+{
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        var field = AccessTools.DeclaredField(typeof(MyEntityCreationThread), "m_thread");
+        return new CodeMatcher(instructions)
+            .MatchStartForward(CodeMatch.StoresField(field))
+            .Insert(new CodeInstruction(OpCodes.Dup),
+                new CodeInstruction(OpCodes.Ldc_I4_1),
+                new CodeInstruction(OpCodes.Callvirt,
+                    AccessTools.PropertySetter(typeof(Thread), nameof(Thread.IsBackground)))
+            )
+            .InstructionEnumeration();
+    }
+}
