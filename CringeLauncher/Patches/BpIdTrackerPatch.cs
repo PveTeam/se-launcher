4a026ff58724495ae77ac0f54fdd4b98e4c639f2
pas2704 <pas2704@outlook.com>
2025-09-18 16:56:28 -0400

    Fix crash from keen's bp id tracker cringe

---
+++ CringeLauncher/Patches/BpIdTrackerPatch.cs
@@ -0,0 +1,12 @@
+﻿using HarmonyLib;
+using Sandbox.Game.Entities;
+
+namespace CringeLauncher.Patches;
+
+
+[HarmonyPatch(typeof(MyBlueprintIdTracker), nameof(MyBlueprintIdTracker.OnAdded))]
+internal static class BpIdTrackerPatch
+{
+    [HarmonyPrefix]
+    public static bool Prefix() => false;
+}
