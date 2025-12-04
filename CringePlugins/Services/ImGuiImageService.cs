aa726067723d826fc2217265ed21cced82287e7b
zznty <94796179+zznty@users.noreply.github.com>
2025-12-04 18:03:12 +0700

    add support for animated images

--- CringePlugins/Services/ImGuiImageService.cs
+++ CringePlugins/Services/ImGuiImageService.cs
@@ -2,12 +2,19 @@
 using System.Net;
 using System.Net.Http.Headers;
 using System.Numerics;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
 using ImGuiNET;
 using NLog;
+using SharpDX;
+using SharpDX.Direct3D;
 using SharpDX.Direct3D11;
 using SharpDX.DXGI;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.Formats.Webp;
+using SixLabors.ImageSharp.PixelFormats;
 using VRage.Collections;
 using Device = SharpDX.Direct3D11.Device;
 
@@ -52,7 +59,7 @@ internal sealed class ImGuiImageService(HttpClient client) : IImGuiImageService
 
         var srv = new ShaderResourceView(device, tex);
 
-        _placeholderImage = new Image(null!, srv, new(1, 1));
+        _placeholderImage = new Image(null!, [(srv, TimeSpan.Zero)], new(1, 1));
     }
 
     internal void Update()
@@ -144,31 +151,150 @@ internal sealed class ImGuiImageService(HttpClient client) : IImGuiImageService
         if (!File.Exists(path))
             throw new FileNotFoundException(null, path);
 
-        using var img = SharpDX.Toolkit.Graphics.Image.Load(path);
+        var img = SixLabors.ImageSharp.Image.Load(path);
 
-        var desc = img.Description;
-        using var tex = new Texture2D(_device, new()
+        try
         {
-            Width = desc.Width,
-            Height = desc.Height,
-            Format = desc.Format,
-            MipLevels = desc.MipLevels,
-            ArraySize = desc.ArraySize,
-            SampleDescription = new()
+            Format format;
+            switch (img)
             {
-                Count = 1
-            },
-            Usage = ResourceUsage.Default,
-            BindFlags = BindFlags.ShaderResource,
-            CpuAccessFlags = CpuAccessFlags.None,
-            OptionFlags = ResourceOptionFlags.None,
-        }, img.ToDataBox());
+                case Image<Bgra32>:
+                    format = Format.B8G8R8A8_UNorm_SRgb;
+                    break;
+                case Image<Rgba32>:
+                    format = Format.R8G8B8A8_UNorm_SRgb;
+                    break;
+                default:
+                {
+                    format = Format.R8G8B8A8_UNorm_SRgb;
+                    var clone = img.CloneAs<Rgba32>();
+                    img.Dispose();
+                    img = clone;
+                    break;
+                }
+            }
 
-        var srv = new ShaderResourceView(_device, tex);
+            var connectingMetadata = img.MetaData.GetWebpMetadata().ToFormatConnectingMetadata();
 
-        image = new Image(identifier, srv, new(desc.Width, desc.Height));
-        _images.Add(identifier, image, true);
-        return image;
+            var data = new DataBox[img.Frames.Count];
+            var frames = new(ShaderResourceView srv, TimeSpan delay)[img.Frames.Count];
+            var previousMetadata = new WebpFrameMetadata[img.Frames.Count];
+            
+            for (var i = 0; i < img.Frames.Count; i++)
+            {
+                var imgFrame = img.Frames[i];
+                var metadata = previousMetadata[i] = imgFrame.Metadata.GetWebpMetadata();
+                frames[i].delay = TimeSpan.FromMilliseconds(metadata.FrameDelay);
+                switch (imgFrame)
+                {
+                    case ImageFrame<Bgra32> imageFrame:
+                    {
+                        CopyData(imageFrame, data, i, connectingMetadata, previousMetadata);
+                        break;
+                    }
+                    case ImageFrame<Rgba32> imageFrame:
+                    {
+                        CopyData(imageFrame, data, i, connectingMetadata, previousMetadata);
+                        break;
+                    }
+                }
+            }
+
+            using var tex = new Texture2D(_device, new()
+            {
+                Width = img.Width,
+                Height = img.Height,
+                Format = format,
+                MipLevels = 1,
+                ArraySize = img.Frames.Count,
+                SampleDescription = new()
+                {
+                    Count = 1
+                },
+                Usage = ResourceUsage.Immutable,
+                BindFlags = BindFlags.ShaderResource,
+                CpuAccessFlags = CpuAccessFlags.None,
+                OptionFlags = ResourceOptionFlags.None,
+            }, data);
+            
+            foreach (var box in data)
+            {
+                if (!box.IsEmpty) Marshal.FreeHGlobal(box.DataPointer);
+            }
+            
+            for (var i = 0; i < frames.Length; i++)
+            {
+                frames[i].srv = new(_device, tex, new()
+                {
+                    Dimension = ShaderResourceViewDimension.Texture2DArray,
+                    Texture2DArray =
+                    {
+                        MipLevels = 1,
+                        ArraySize = 1,
+                        FirstArraySlice = i,
+                    }
+                });
+            }
+
+            image = new Image(identifier, frames, new(img.Width, img.Height));
+            _images.Add(identifier, image, true);
+            return image;
+        }
+        finally
+        {
+            img.Dispose();
+        }
+    }
+
+    private static unsafe void CopyData<T>(ImageFrame<T> frame, DataBox[] data, int frameIndex,
+        FormatConnectingMetadata connectingMetadata, WebpFrameMetadata[] previousMetadata) where T : unmanaged, IPixel<T>
+    {
+        var cb = frame.Width * frame.Height;
+        data[frameIndex] = new()
+        {
+            DataPointer = Marshal.AllocHGlobal(cb * sizeof(T)),
+            RowPitch = frame.Width * sizeof(T)
+        };
+        var destination = new Span<T>((void*)data[frameIndex].DataPointer, cb);
+
+        if (frameIndex > 0)
+        {
+            switch (previousMetadata[frameIndex - 1].DisposalMode)
+            {
+                case FrameDisposalMode.Unspecified:
+                    break;
+                case FrameDisposalMode.DoNotDispose:
+                {
+                    var previousFrame = new Span<T>((void*)data[frameIndex - 1].DataPointer, cb);
+                    previousFrame.CopyTo(destination);
+                    break;
+                }
+                case FrameDisposalMode.RestoreToBackground:
+                    destination.Fill(connectingMetadata.BackgroundColor.ToPixel<T>());
+                    break;
+                case FrameDisposalMode.RestoreToPrevious:
+                    if (frameIndex > 1)
+                    {
+                        var previousFrame = new Span<T>((void*)data[frameIndex - 2].DataPointer, cb);
+                        previousFrame.CopyTo(destination);
+                    }
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        else
+        {
+            destination.Fill(connectingMetadata.BackgroundColor.ToPixel<T>());
+        }
+        
+        if (previousMetadata[frameIndex].BlendMode == FrameBlendMode.Over && frameIndex > 0)
+        {
+            var previousFrame = new Span<T>((void*)data[frameIndex - 1].DataPointer, cb);
+            previousFrame.CopyTo(destination);
+        }
+
+        frame.CopyPixelDataTo(destination);
     }
 
     private class ImageReference(ImGuiImage placeholderImage) : ImGuiImage
@@ -186,10 +312,12 @@ internal sealed class ImGuiImageService(HttpClient client) : IImGuiImageService
         }
     }
 
-    private class Image(ImageIdentifier identifier, ShaderResourceView srv, Vector2 size) : ImGuiImage
+    private class Image(ImageIdentifier identifier, (ShaderResourceView srv, TimeSpan delay)[] srvs, Vector2 size) : ImGuiImage
     {
         private bool _disposed;
         private long _lastUse = Stopwatch.GetTimestamp();
+        private long _lastFrame = Stopwatch.GetTimestamp();
+        private int _frame;
 
         public override ImTextureRef TextureId
         {
@@ -198,7 +326,7 @@ internal sealed class ImGuiImageService(HttpClient client) : IImGuiImageService
                 OnUse();
                 return new()
                 {
-                    _TexID = srv.NativePointer
+                    _TexID = srvs[_frame].srv.NativePointer
                 };
             }
         }
@@ -217,6 +345,12 @@ internal sealed class ImGuiImageService(HttpClient client) : IImGuiImageService
         private void OnUse()
         {
             ObjectDisposedException.ThrowIf(_disposed, this);
+            if (srvs.Length > 1 && Stopwatch.GetElapsedTime(_lastFrame) >= srvs[_frame].delay)
+            {
+                _frame++;
+                if (_frame >= srvs.Length) _frame = 0;
+                _lastFrame = Stopwatch.GetTimestamp();
+            }
             _lastUse = Stopwatch.GetTimestamp();
         }
 
@@ -224,12 +358,15 @@ internal sealed class ImGuiImageService(HttpClient client) : IImGuiImageService
         {
             if (_disposed) return;
             _disposed = true;
-            srv.Dispose();
+            foreach (var (srv, _) in srvs)
+            {
+                srv.Dispose();
+            }
         }
 
         public override string ToString()
         {
-            return $"Image {{ {identifier} {size} }}";
+            return $"Image {{ {identifier} {size} x{srvs.Length} }}";
         }
     }
 
