427921a4da3da52ce9215ad4ede611ee52690e04
zznty <94796179+zznty@users.noreply.github.com>
2025-12-05 01:22:56 +0700

    fix transformer

--- CringeBootstrap/Transformers/Impl/SteamworksTransformer.cs
+++ CringeBootstrap/Transformers/Impl/SteamworksTransformer.cs
@@ -26,7 +26,24 @@ internal class SteamworksTransformer : ITransformer
             return false;
         }
         
-        var instructions = ctor.Body.Instructions;
+        if (!Remove(ctor)) return false;
+
+        var method = typeDefinition.FindMethod("LoadStats");
+        
+        if (method is null)
+        {
+            Log.Warn("Failed to find steam service LoadStats");
+            return false;
+        }
+        
+        if (!Remove(method)) return false;
+
+        return true;
+    }
+
+    private static bool Remove(MethodDef method)
+    {
+        var instructions = method.Body.Instructions;
         var index = -1;
         for (var i = 0; i < instructions.Count; i++)
         {
@@ -46,7 +63,6 @@ internal class SteamworksTransformer : ITransformer
         
         instructions[index] = Instruction.Create(OpCodes.Nop); // call
         instructions[index + 1] = Instruction.Create(OpCodes.Nop); // pop
-
         return true;
     }
 }
